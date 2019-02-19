bd3dc9d97233d07eaab42ede345a7fbe7fa23123
Marvin Frachet <marvin.frachet@gmail.com>
2019-02-19 11:46:42 -0500

    mocks seem good

--- patch_test.go
+++ patch_test.go
@@ -2,11 +2,17 @@ package vn_test
 
 import (
 	"testing"
+
+	"github.com/golang/mock/gomock"
+	vn "github.com/mfrachet/go-vdom-wasm"
+	"github.com/mfrachet/go-vdom-wasm/mock"
 )
 
 func TestAppendChild(t *testing.T) {
-	// ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 
-	// mockVNode := mock.NewMockNode(ctrl)
-	// mockDNode := mock.NewMockDomNode(ctrl)
+	mockVNode := mock.NewMockNode(ctrl)
+	mockDNode := mock.NewMockDomNode(ctrl)
+
+	vn.AppendToNode(mockDNode, mockVNode)
 }
