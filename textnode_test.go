5a987ced58abfda478ff40aa53db10968fd1d847
Marvin Frachet <marvin.frachet@gmail.com>
2019-02-22 14:35:52 -0500

    fixed

--- textnode_test.go
+++ textnode_test.go
@@ -1,16 +1,31 @@
-package vn
+package vn_test
 
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	vn "github.com/mfrachet/go-vdom-wasm"
+	"github.com/mfrachet/go-vdom-wasm/mock"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTextNode_IsSame(t *testing.T) {
-	a := &TextNode{Value: "Hello world"}
-	b := &TextNode{Value: "Hello world"}
-	c := &TextNode{Value: "Hello world2"}
+	a := &vn.TextNode{Value: "Hello world"}
+	b := &vn.TextNode{Value: "Hello world"}
+	c := &vn.TextNode{Value: "Hello world2"}
 
 	assert.Equal(t, true, a.IsSame(b))
 	assert.Equal(t, false, a.IsSame(c))
+}
+
+func TestTextNode_SetElement(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	a := &vn.TextNode{Value: "Hello world"}
+
+	mockDomNode := mock.NewMockDomNode(ctrl)
+	a.SetElement(mockDomNode)
+
+	assert.Equal(t, mockDomNode, a.Element)
 }
