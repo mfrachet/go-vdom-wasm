34a9b3bb531c9b491fdcc3aca4ab684e7d898f5d
Marvin Frachet <marvin.frachet@gmail.com>
2019-02-25 10:00:42 -0500

    fix

--- h_test.go
+++ h_test.go
@@ -9,7 +9,7 @@ import (
 import vn "github.com/mfrachet/go-vdom-wasm"
 
 func TestH_WithText(t *testing.T) {
-	expected := vn.NewNode("div", &vn.Attrs{Props: &vn.Props{}, Events: &vn.Ev{}}, nil, &vn.TextNode{Value: "Hello world", Element: nil}, nil, nil)
+	expected := vn.NewNode("div", &vn.Attrs{Props: &vn.Props{}, Events: &vn.Ev{}}, nil, vn.NewTextnode("Hello world"), nil, nil)
 
 	vNode := vn.H("div", nil, "Hello world")
 
@@ -26,12 +26,12 @@ func TestH_WithChildren(t *testing.T) {
 }
 
 func TestH_WithAttributes(t *testing.T) {
-	exectedWithoutAttrs := vn.NewNode("div", &vn.Attrs{Props: &vn.Props{}, Events: &vn.Ev{}}, nil, &vn.TextNode{Value: "Hello world", Element: nil}, nil, nil)
+	exectedWithoutAttrs := vn.NewNode("div", &vn.Attrs{Props: &vn.Props{}, Events: &vn.Ev{}}, nil, vn.NewTextnode("Hello world"), nil, nil)
 	vNodeWithoutAttrs := vn.H("div", nil, "Hello world")
 
 	assert.Equal(t, exectedWithoutAttrs, vNodeWithoutAttrs)
 
-	exectedWithAttrs := vn.NewNode("div", &vn.Attrs{Props: &vn.Props{"class": "navbar"}, Events: &vn.Ev{}}, nil, &vn.TextNode{Value: "Hello world", Element: nil}, nil, nil)
+	exectedWithAttrs := vn.NewNode("div", &vn.Attrs{Props: &vn.Props{"class": "navbar"}, Events: &vn.Ev{}}, nil, vn.NewTextnode("Hello world"), nil, nil)
 	vNodeWithAttrs := vn.H("div", &vn.Attrs{Props: &vn.Props{"class": "navbar"}}, "Hello world")
 
 	assert.Equal(t, exectedWithAttrs, vNodeWithAttrs)
