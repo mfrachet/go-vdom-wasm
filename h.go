ff1226900de83918a3a40cc61c62d887a9654a02
Marvin Frachet <marvin.frachet@gmail.com>
2019-02-25 11:14:39 -0500

    switch interface

--- h.go
+++ h.go
@@ -1,20 +1,20 @@
 package vn
 
-func H(tagName string, attrs *Attrs, variadicChildren interface{}, params ...interface{}) Node {
+func H(tagName string, attrs *Attrs, params ...interface{}) Node {
 	sanitizedAttrs := Sanitize(attrs)
 	var key KeyIdentifier
 	var children Children
 	var textNode *TextNode
 
-	if len(params) == 1 {
-		key = params[0].(KeyIdentifier)
-	}
-
-	switch (variadicChildren).(type) {
-	case string:
-		textNode = NewTextnode((variadicChildren).(string))
-	default:
-		children = variadicChildren.(Children)
+	for _, param := range params {
+		switch (param).(type) {
+		case string:
+			textNode = NewTextnode((param).(string))
+		case KeyIdentifier:
+			key = param.(KeyIdentifier)
+		default:
+			children = param.(Children)
+		}
 	}
 
 	return NewNode(tagName, sanitizedAttrs, children, textNode, nil, key)
